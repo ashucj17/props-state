79370772d2e3fac7db1afcbc2d504b50030c3861
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-04-16 12:21:28 +0530

    useState() counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 function App(){
+  let [counter, setCounter] = useState(0)
+
+  const addCounter = () =>{
+    setCounter(counter + 1)
+  }
+
+  let removeCounter = ()=>{
+    setCounter(counter -1)
+  }
+
+  const username  = 'props and state'
   return(
     <div><h1>
-      Props and state</h1>
+      {username}</h1>
+      <button onClick={addCounter}>Add Counter {counter}</button>
+      <button onClick={removeCounter}>Remove Counter {counter}</button>
       </div>
   )
 }
